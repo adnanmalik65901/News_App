fb49e7f97bce147db1bdf84e6c23588858e72d69
Adnan Malik <adnanmalik65901@gmail.com>
2021-08-04 19:21:45 -0700

    Initial commit

---
+++ app/src/main/java/com/example/news_app/ui/gallery/GalleryFragment.java
@@ -0,0 +1,34 @@
+package com.example.news_app.ui.gallery;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.news_app.R;
+import com.example.news_app.WebViewController;
+
+public class GalleryFragment extends Fragment {
+
+    private GalleryViewModel galleryViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        galleryViewModel =
+                new ViewModelProvider(this).get(GalleryViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+        WebView webView=root.findViewById(R.id.webViewAljazeera);
+        webView.loadUrl("https://www.aljazeera.com/");
+        webView.setWebViewClient(new WebViewController());
+
+        return root;
+    }
+}
---
+++ app/src/main/java/com/example/news_app/ui/home/HomeFragment.java
@@ -0,0 +1,33 @@
+package com.example.news_app.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.news_app.R;
+import com.example.news_app.WebViewController;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        WebView webView=root.findViewById(R.id.webViewBBC);
+        webView.loadUrl("https://www.bbc.com/urdu");
+        webView.setWebViewClient(new WebViewController());
+        return root;
+    }
+}
